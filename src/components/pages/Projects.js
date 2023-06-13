e286ec95ed5be0b75af4d7c7b3c713561a501a08
Tanner Shadday <tshadday99@gmail.com>
2023-06-13 19:48:18 -0400

    fixed mobile sizing for projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -7,33 +7,29 @@ function Projects() {
         <div className="content">
             <h1>My Projects</h1>
 
-            <div className='container'>
-                <div className='row'>
-                    <div className='project col m-3' id='Coeus'>
-                        <a href="https://Coeus.chat" target="_blank" rel="noreferrer">
-                            <p>Coeus by Data Fusion Specialists</p>
-                        </a>
-                    </div>
-
-                    <div className='project col m-3' id='Troogle'>
-                        <a href="https://gabefando.github.io/travel-website/index.html" target="_blank" rel="noreferrer">
-                            <p>Troogle: A Travel Website</p>
-                        </a>
-                    </div>
+            <div className='container d-flex flex-wrap justify-content-center'>
+                <div className='project m-3' id='Coeus'>
+                    <a href="https://Coeus.chat" target="_blank" rel="noreferrer">
+                        <p>Coeus by Data Fusion Specialists</p>
+                    </a>
                 </div>
-                
-                <div className='row'>
-                    <div className='project col m-3' id='UnsubHub'>
-                        <a href="https://github.com/tshadday/Subscription-Tracker-Service" target="_blank" rel="noreferrer">
-                            <p>UnsubHub: A Subscription Tracker Service</p>
-                        </a>
-                    </div>
 
-                    <div className='project col m-3' id='Paws-N-Play'>
-                        <a href="https://github.com/tshadday/Paws-N-Play" target="_blank" rel="noreferrer">
-                            <p>Paws N Play: A Pet Playdate Finder App</p>
-                        </a>
-                    </div>
+                <div className='project m-3' id='Troogle'>
+                    <a href="https://gabefando.github.io/travel-website/index.html" target="_blank" rel="noreferrer">
+                        <p>Troogle: A Travel Website</p>
+                    </a>
+                </div>
+            
+                <div className='project m-3' id='UnsubHub'>
+                    <a href="https://github.com/tshadday/Subscription-Tracker-Service" target="_blank" rel="noreferrer">
+                        <p>UnsubHub: A Subscription Tracker Service</p>
+                    </a>
+                </div>
+
+                <div className='project m-3' id='Paws-N-Play'>
+                    <a href="https://github.com/tshadday/Paws-N-Play" target="_blank" rel="noreferrer">
+                        <p>Paws N Play: A Pet Playdate Finder App</p>
+                    </a>
                 </div>
             </div>
             <Footer />
